ffb7884982cccedd99da4b3fd82c2ca2f14b6a9b
CodeVision <codevision@gmail.com>
2022-08-28 15:55:03 +0200

    refactor(nvim): rename keymappings

--- neovim/.config/nvim/lua/codevision/mappings.lua
+++ neovim/.config/nvim/lua/codevision/mappings.lua
@@ -2,13 +2,13 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
-local mapkey = vim.keymap.set
-mapkey('n', '<F3>', ':source %<CR>', { silent = true })
+local keymap = vim.keymap.set
+keymap('n', '<F3>', ':source %<CR>', { silent = true })
 
-mapkey('!', 'jj', '<Esc>', { noremap = true })
+keymap('!', 'jj', '<Esc>', { noremap = true })
 
-mapkey('n', '<leader>b', '<c-^>', { noremap = true })
+keymap('n', '<leader>b', '<c-^>', { noremap = true })
 
--- vim.keymap.set({ 'n', 'v' }, 'k', 'gk')
--- vim.keymap.set({ 'n', 'v' }, 'j', 'gj')
+-- keymap({ 'n', 'v' }, 'k', 'gk')
+-- keymap({ 'n', 'v' }, 'j', 'gj')
 
