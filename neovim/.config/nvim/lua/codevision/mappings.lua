5e019108393d06042ddafa30222a7fda0c611129
CodeVision <codevision@gmail.com>
2022-09-11 17:46:13 +0200

    config(nvim): add keybina and reorder requires

--- neovim/.config/nvim/lua/codevision/mappings.lua
+++ neovim/.config/nvim/lua/codevision/mappings.lua
@@ -8,6 +8,7 @@ keymap('n', '<F3>', ':source %<CR>', { silent = true })
 keymap('!', 'jj', '<Esc>', { noremap = true })
 
 keymap('n', '<leader>b', '<c-^>', { noremap = true })
+keymap('n', '<leader>q', ':bd<cr>', { noremap = true })
 
 -- keymap({ 'n', 'v' }, 'k', 'gk')
 -- keymap({ 'n', 'v' }, 'j', 'gj')
