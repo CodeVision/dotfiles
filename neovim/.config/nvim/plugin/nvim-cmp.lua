aef524d23f5046e41505b9be00d14a64d075a8e4
CodeVision <codevision@gmail.com>
2022-09-11 18:02:38 +0200

    config(nvim): configure completion menu

--- neovim/.config/nvim/plugin/nvim-cmp.lua
+++ neovim/.config/nvim/plugin/nvim-cmp.lua
@@ -1,6 +1,20 @@
 local cmp = require('cmp')
 local lspkind = require('lspkind')
 
+-- add completion highlights
+local highlight = vim.api.nvim_set_hl
+highlight(0, 'CmpItemAbbrDeprecated', { bg='NONE', strikethrough = true, fg='#808080' })
+highlight(0, 'CmpItemAbbrMatch', { bg='NONE', fg='#569CD6' })
+highlight(0, 'CmpItemAbbrMatchFuzzy', { bg='NONE', fg='#569CD6' })
+highlight(0, 'CmpItemKindVariable', { bg='NONE', fg='#9CDCFE' })
+highlight(0, 'CmpItemKindInterface', { bg='NONE', fg='#9CDCFE' })
+highlight(0, 'CmpItemKindText', { bg='NONE', fg='#9CDCFE' })
+highlight(0, 'CmpItemKindFunction', { bg='NONE', fg='#C586C0' })
+highlight(0, 'CmpItemKindMethod', { bg='NONE', fg='#C586C0' })
+highlight(0, 'CmpItemKindKeyword', { bg='NONE', fg='#D4D4D4' })
+highlight(0, 'CmpItemKindProperty', { bg='NONE', fg='#D4D4D4' })
+highlight(0, 'CmpItemKindUnit', { bg='NONE', fg='#D4D4D4' })
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -9,32 +23,53 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
     ['<c-space>'] = cmp.mapping.complete(),
-    ['<cr>'] = cmp.mapping.confirm({ select = true }),
+    ['<c-b>'] = cmp.mapping.scroll_docs(4),
+    ['<c-f>'] = cmp.mapping.scroll_docs(-4),
+    ['<c-y'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true
+    }
+    -- ['<cr>'] = cmp.mapping.confirm({ select = true }),
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
     { name = 'nvim_lua' },
+    { name = 'nvim_lsp' },
+    { name = 'path' },
     { name = 'luasnip' },
-    { name = 'buffer' },
-    { name = 'path' }
+  }, {
+    { name = 'buffer', keyword_length = 4 },
   }),
   formatting = {
-    fields = { 'menu', 'abbr', 'kind' },
-    format = lspkind.cmp_format({
-      mode = 'symbol_text',
-      maxwidth = 50,
-      menu = ({
-        nvim_lsp = 'λ',
-        nvim_lua = '☾',
-        luasnip = '⋗',
-        buffer = 'Ω',
-        path = '🖫'
-      }),
+    fields = { 'abbr', 'kind' }, -- removed menu for rust analyzer
+    format = function(entry, item)
+      local kind = lspkind.cmp_format({
+        mode = 'symbol_text',
+        preset = 'codicons',
+        maxwidth = 40,
+      })(entry, item)
+      -- local source_icons = {
+      --   nvim_lsp = 'λ',
+      --   nvim_lua = '☾',
+      --   luasnip = '⋗',
+      --   buffer = 'Ω',
+      --   path = '🖫'
+      -- }
+      local source_name_map = {
+        buffer = "[buf]",
+        nvim_lsp = "[lsp]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        luasnip = "[snip]"
+      }
 
-      before = function(entry, item)
-        item.dup = ({ nvim_lua = 0 })[entry.source.name] or 0
-        return item
-      end
-    })
+      local kind_string = vim.split(kind.kind, '%s', { trimempty = true })
+      kind.kind = string.format("\t\t%s %-10s%7s", kind_string[1], kind_string[2], source_name_map[entry.source.name])
+
+      item.dup = ({ nvim_lua = 0 })[entry.source.name] or 0
+      return item
+    end
+  },
+  experimental = {
+    ghost_text = { hl_group = 'NvimTreeIndentMarker' }
   }
 })
