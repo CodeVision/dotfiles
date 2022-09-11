9afbb71f8f1f3da410ee601972f5a07f7dd29dcb
CodeVision <codevision@gmail.com>
2022-09-11 18:00:17 +0200

    config(nvim): only use local eslint for lsp

--- neovim/.config/nvim/lua/codevision/lsp/init.lua
+++ neovim/.config/nvim/lua/codevision/lsp/init.lua
@@ -7,7 +7,8 @@ local nls = require('null-ls')
 nls.setup({
   sources = {
     nls.builtins.diagnostics.eslint.with({
-      diagnostics_format = "[#{c}] #{m} (#{s})"
+      diagnostics_format = "[#{c}] #{m} (#{s})",
+      only_local = "node_modules/.bin"
     })
   }
 })
