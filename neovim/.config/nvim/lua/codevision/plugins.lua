e9093815f5aa0eabe797acee74ae052531900aaf
CodeVision <codevision@gmail.com>
2022-10-21 19:46:52 +0200

    nvim: add back in plugins

--- neovim/.config/nvim/lua/codevision/plugins.lua
+++ neovim/.config/nvim/lua/codevision/plugins.lua
@@ -31,75 +31,34 @@ local startup = function(use)
     config = load_config('telescope-nvim')
   }
   use { 'nvim-telescope/telescope-fzy-native.nvim', run = 'make' }
- --
- --  -- coding
- --  use {
- --    "windwp/nvim-autopairs",
- --    config = function() require("nvim-autopairs").setup({}) end
- --  }
- --  use {
- --    "kylechui/nvim-surround",
- --    config = function() require("nvim-surround").setup({}) end
- --  }
- --  use {
- --    'AndrewRadev/splitjoin.vim',
- --    keys = { "gJ", "gS" }
- --  }
- --  use 'NvChad/nvim-colorizer.lua'
- use {
-   'numToStr/Comment.nvim',
-   config = function() require("Comment").setup({}) end
- }
- --  use 'gpanders/editorconfig.nvim'
- --  -- use {
- --  --   'nvim-neotest/neotest',
- --  --   requires = {
- --  --     'nvim-lua/plenary.nvim',
- --  --     'nvim-treesitter/nvim-treesitter',
- --  --     'antoinemadec/FixCursorHold.nvim',
- --  --     'haydenmeade/neotest-jest'
- --  --   }
- --  -- }
- --  use {
- --    'folke/trouble.nvim',
- --    requires = 'nvim-tree/nvim-web-devicons',
- --  }
- --
- --  use { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' }
- --  use {
- --    'nvim-treesitter/nvim-treesitter',
- --    run = ':TSUpdate',
- --  }
- --  use { 'nvim-treesitter/playground' }
- --
- --  use 'neovim/nvim-lspconfig'
- --  use 'jose-elias-alvarez/null-ls.nvim'
- --  use 'onsails/lspkind.nvim'
- --  use 'glepnir/lspsaga.nvim'
- --  use {
- --    'j-hui/fidget.nvim',
- --    config = function() require('fidget').setup({}) end
- --  }
- --
- --  -- completion
- --  use 'hrsh7th/nvim-cmp'
- --  use 'hrsh7th/cmp-buffer'
- --  use 'hrsh7th/cmp-path'
- --  -- use 'hrsh7th/cmp-nvim-lua'
- --  use 'hrsh7th/cmp-nvim-lsp'
- --
- --  -- snippets
- --  use 'L3MON4D3/LuaSnip'
- --  use 'saadparwaiz1/cmp_luasnip'
- --
- --  -- languages
- --  use 'simrat39/rust-tools.nvim'
- --  use { 'folke/neodev.nvim',
- --    config = function() require('neodev').setup({}) end
- --  }
- --  use 'mattn/emmet-vim'
- --
- --  -- interface
+  use {
+    "kylechui/nvim-surround",
+    config = function() require("nvim-surround").setup({}) end
+  }
+
+  -- coding
+  use {
+    'AndrewRadev/splitjoin.vim',
+    keys = { "gJ", "gS" }
+  }
+  use {
+    'numToStr/Comment.nvim',
+    config = function() require("Comment").setup({}) end
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = load_config('nvim-treesitter')
+  }
+  --  use { 'nvim-treesitter/playground' }
+  --  use { 'nvim-treesitter/nvim-treesitter-textobjects' }
+  use {
+    'kevinhwang91/nvim-ufo',
+    requires = 'kevinhwang91/promise-async',
+    config = load_config(' nvim-ufo'),
+  }
+
+  -- interface
   use {
     'nvim-neo-tree/neo-tree.nvim',
     requires = {
@@ -112,49 +71,12 @@ local startup = function(use)
   use {
     'mrjones2014/smart-splits.nvim',
     config = load_config('smart-splits-nvim')
-
   }
- --  use {
- --    'nvim-tree/nvim-tree.lua',
- --    requires = {
- --      'nvim-tree/nvim-web-devicons',
- --    },
- --    tag = 'nightly'
- --  }
- --  use {
- --    'romgrk/barbar.nvim',
- --    requires = {
- --      'nvim-tree/nvim-web-devicons'
- --    },
- --  }
- --  use 'vim-airline/vim-airline'
- --
- --  use 'folke/tokyonight.nvim'
   use {
     'Mofiqul/vscode.nvim',
     config = load_config('vscode-nvim')
   }
- --  use {
- --    'tomasiser/vim-code-dark',
- --    run = 'cp autoload/airline/themes/codedark.vim ../vim-airline/autoload/airline/themes'
- --  }
- -- 
- --  use {
- --    'nvim-tree/nvim-web-devicons',
- --    config = function()
- --      require('nvim-web-devicons').setup({
- --        color_icons = true,
- --        default = true,
- --      })
- --    end
- --  }
- --
- --  -- plugin development
- --  use {
- --    'tpope/vim-scriptease',
- --    cmd = 'Messages'
- --  }
- --  use '/home/codevision/dev/projects/nvim-executioner'
+
   if packer_bootstrap then
     require('packer').sync()
   end
---
+++ neovim/.config/nvim/lua/codevision/plugins/nvim-ufo.lua
@@ -0,0 +1,27 @@
+return function()
+  -- folds
+  local options = vim.opt
+  options.foldcolumn = '1'
+  options.foldlevel = 99
+  options.foldlevelstart = 99
+  options.foldenable = true
+  options.fillchars = [[eob: ,fold: ,foldopen:⮟,foldsep:|,foldclose:⮞]]
+
+  local ufo = require('ufo')
+
+  vim.keymap.set('n', 'zR', ufo.openAllFolds)
+  vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+  vim.keymap.set('n', 'K', function()
+    local winid = ufo.peekFoldedLinesUnderCursor()
+    if not winid then
+      vim.lsp.buf.hover()
+    end
+  end)
+
+  ufo.setup({
+    provider_selector = function (--[[ bufnr, filetype, buftype ]])
+      return { 'treesitter', 'indent' }
+    end
+  })
+end
+
