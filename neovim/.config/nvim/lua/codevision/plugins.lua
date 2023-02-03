68d2e2c9ff2ac9e0bcdf5080d65c8f7a031626c8
CodeVision <codevision@gmail.com>
2023-02-03 15:06:02 +0100

    neovim: replace packer with lazy

--- neovim/.config/nvim/lua/codevision/plugins.lua
+++ neovim/.config/nvim/lua/codevision/plugins.lua
@@ -1,15 +1,15 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .."/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
-
-local packer_bootstrap = ensure_packer()
+vim.opt.rtp:prepend(lazypath)
 
 local configs = {}
 local load_config = function()
@@ -29,9 +29,9 @@ local add_config = function(config)
   table.insert(configs, config)
 end
 
-local setup = {}
+local setups = {}
 local perform_setup = function()
-  for _, v in ipairs(setup) do
+  for _, v in ipairs(setups) do
     local success, module = pcall(require, v)
     if success and module then
       module.setup({})
@@ -42,7 +42,7 @@ local perform_setup = function()
 end
 
 local add_setup = function(name)
-  table.insert(setup, name)
+  table.insert(setups, name)
 end
 
 local configure = function()
@@ -61,111 +61,115 @@ local get_name = function(plugin)
   return vim.fs.basename(plugin_name)
 end
 
-local startup = function(use)
-  local config_use = function(plugin, config_file)
-    use(plugin)
-
+local startup = function()
+  local config = function(plugin, config_file)
     if config_file == nil then
       local config_files = vim.fn.fnamemodify(get_name(plugin):lower(), ':r')
       add_config(config_files)
     else
       add_config(config_file)
     end
+
+    return { plugin }
   end
 
-  local setup_use = function(plugin, name)
-    use(plugin)
-
+  local setup = function(plugin, name)
     if name == nil then
       add_setup(vim.fn.fnamemodify(get_name(plugin), ':r'))
     else
       add_setup(name)
     end
+
+    return { plugin }
   end
 
-  --  plugins
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lua/plenary.nvim'
+  return {
+    --  plugins
+    'wbthomason/packer.nvim',
+    'nvim-lua/plenary.nvim',
 
-  -- files
-  config_use({
-    'nvim-telescope/telescope.nvim',
-    requires = 'nvim-lua/plenary.nvim',
-    tag = '0.1.0',
-  }, 'telescope-nvim')
-  use { 'nvim-telescope/telescope-fzy-native.nvim', run = 'make' }
-  setup_use('kylechui/nvim-surround')
+    -- files
+    config({
+      'nvim-telescope/telescope.nvim',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      tag = '0.1.0',
+    }, 'telescope-nvim'),
+    { 'nvim-telescope/telescope-fzy-native.nvim', build = 'make' },
+    setup('kylechui/nvim-surround'),
 
-  -- coding
-  use 'gpanders/editorconfig.nvim'
-  setup_use('windwp/nvim-autopairs')
-  use { 'AndrewRadev/splitjoin.vim', keys = { 'gJ', 'gS' } }
-  setup_use('numToStr/Comment.nvim')
-  config_use({
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-  })
-  --  use { 'nvim-treesitter/playground' }
-  --  use { 'nvim-treesitter/nvim-treesitter-textobjects' }
-  config_use('NvChad/nvim-colorizer.lua')
-  config_use({
-    'folke/trouble.nvim',
-    requires = 'nvim-tree/nvim-web-devicons',
-  }, 'trouble-nvim')
+    -- coding
+    setup('windwp/nvim-autopairs'),
+    -- { 'AndrewRadev/splitjoin.vim' },
+    setup('numToStr/Comment.nvim'),
+    config({
+      'nvim-treesitter/nvim-treesitter',
+      build = ':TSUpdate',
+    }),
+    'RRethy/nvim-treesitter-endwise',
 
-  -- completion
-  config_use('hrsh7th/nvim-cmp')
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-nvim-lsp'
+    --  { 'nvim-treesitter/playground' }
+    --  { 'nvim-treesitter/nvim-treesitter-textobjects' }
+    config('NvChad/nvim-colorizer.lua'),
+    config({
+      'folke/trouble.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+    }, 'trouble-nvim'),
 
-  config_use('neovim/nvim-lspconfig', 'codevision.lsp')
-  use 'onsails/lspkind.nvim'
-  config_use('glepnir/lspsaga.nvim', 'lspsaga-nvim')
-  use 'jose-elias-alvarez/null-ls.nvim'
-  setup_use('j-hui/fidget.nvim')
-  config_use({
-    'kevinhwang91/nvim-ufo',
-    requires = 'kevinhwang91/promise-async',
-  })
+    -- completion
+    config('hrsh7th/nvim-cmp'),
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-nvim-lsp',
 
-  -- snippets
-  config_use('L3MON4D3/LuaSnip')
-  use 'saadparwaiz1/cmp_luasnip'
+    config('neovim/nvim-lspconfig', 'codevision.lsp'),
+    'onsails/lspkind.nvim',
+    config({
+      'glepnir/lspsaga.nvim',
+      event = 'BufRead',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+    }, 'lspsaga-nvim'),
+    'jose-elias-alvarez/null-ls.nvim',
 
-  -- languages
-  use 'stevearc/vim-arduino'
+    -- snippets
+    config('L3MON4D3/LuaSnip'),
+    'saadparwaiz1/cmp_luasnip',
 
-  -- interface
-  config_use({
-    'nvim-neo-tree/neo-tree.nvim',
-    requires = {
-      'nvim-tree/nvim-web-devicons',
-      'MunifTanjim/nui.nvim'
+    -- languages
+    'stevearc/vim-arduino',
+    'simrat39/rust-tools.nvim',
+    'vim-ruby/vim-ruby',
+
+    -- interface
+    setup('j-hui/fidget.nvim'),
+    config({
+      'kevinhwang91/nvim-ufo',
+      dependencies = { 'kevinhwang91/promise-async' },
+    }),
+    config('luukvbaal/statuscol.nvim', 'statuscol-nvim'),
+    config({
+      'nvim-neo-tree/neo-tree.nvim',
+      dependencies = {
+        'nvim-tree/nvim-web-devicons',
+        'MunifTanjim/nui.nvim'
+      },
+      branch = 'v2.x',
+    }, 'neo-tree-nvim'),
+    config({
+      'romgrk/barbar.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+    }, 'barbar-nvim'),
+    config('vim-airline/vim-airline'),
+    {
+      'tomasiser/vim-code-dark',
+      build = 'cp autoload/airline/themes/codedark.vim ../vim-airline/autoload/airline/themes'
     },
-    branch = 'v2.x',
-  }, 'neo-tree-nvim')
-  config_use({
-    'romgrk/barbar.nvim',
-    requires = 'nvim-tree/nvim-web-devicons',
-  }, 'barbar-nvim')
-  config_use('vim-airline/vim-airline')
-  use {
-    'tomasiser/vim-code-dark',
-    run = 'cp autoload/airline/themes/codedark.vim ../vim-airline/autoload/airline/themes'
+    config('mrjones2014/smart-splits.nvim', 'smart-splits-nvim'),
+    config('Mofiqul/vscode.nvim', 'vscode-nvim'),
   }
-  config_use('mrjones2014/smart-splits.nvim', 'smart-splits-nvim')
-  config_use('Mofiqul/vscode.nvim', 'vscode-nvim')
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
 end
 
-local packer = require('packer').startup({
-  startup
-})
+local lazy = require('lazy').setup(startup())
 
 configure()
 
-return packer
+return lazy
