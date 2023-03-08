b5dc24affdcb37f4e25dc036114dddf28eff1778
CodeVision <codevision@gmail.com>
2023-03-08 13:55:19 +0100

    nvim: change lua lsp setup

--- neovim/.config/nvim/lua/codevision/lsp/servers/lua.lua
+++ neovim/.config/nvim/lua/codevision/lsp/servers/lua.lua
@@ -1,7 +1,7 @@
 local capabilities = require('codevision.lsp.util').capabilities
 local on_attach = require('codevision.lsp.util').on_attach
 
-require('lspconfig').sumneko_lua.setup({
+require('lspconfig').lua_ls.setup({
   capabilities = capabilities,
   on_attach = on_attach,
   settings = {
