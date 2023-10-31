6c0108a442b35cae3438c66648e8df694ab1acd1
CodeVision <codevision@gmail.com>
2023-10-31 19:43:46 +0100

    config(neovim): change mappings

--- neovim/.config/nvim/lua/codevision/mappings.lua
+++ neovim/.config/nvim/lua/codevision/mappings.lua
@@ -2,11 +2,13 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
+
 vim.keymap.set('n', '<F3>', ':source %<CR>', { silent = true })
 
 vim.keymap.set('!', 'jj', '<Esc>')
 
 vim.keymap.set('n', '<leader>b', '<c-^>')
+vim.keymap.set('n', '<leader>em', ':messages<CR>')
 
 -- keymap({ 'n', 'v' }, 'k', 'gk')
 -- keymap({ 'n', 'v' }, 'j', 'gj')
@@ -17,7 +19,9 @@ vim.keymap.set('n', '<C-o>', function()
   local start_buffer_id = vim.fn.bufnr('%')
   vim.cmd('execute "normal \\<F11>"')
   local current_buffer = vim.api.nvim_buf_get_name(0)
-  if start_buffer_name ~= current_buffer then
+  if start_buffer_name ~= current_buffer and start_buffer_id then
     vim.api.nvim_buf_delete(start_buffer_id, {})
   end
 end, { silent = true })
+
+vim.g.user_emmet_leader_key='<C-t>'
