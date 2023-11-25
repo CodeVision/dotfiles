a3f42085c949a67dbd451cc8c5794161741d94dd
CodeVision <codevision@gmail.com>
2023-11-25 09:20:31 +0100

    config(neovim): add basic go settings

--- neovim/.config/nvim/lua/codevision/lsp/servers/go.lua
+++ neovim/.config/nvim/lua/codevision/lsp/servers/go.lua
@@ -1,7 +1,17 @@
 local capabilities = require('codevision.lsp.util').capabilities
-local on_attach = require('codevision.lsp.util').on_attach
 
-require('lspconfig').gopls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'go', 'gomod', 'gowork', 'gotmpl' },
+  callback = function()
+    P('starting go pls')
+    local executable = vim.fn.fnamemodify(vim.env.GOPATH, ':p:h') .. '/bin/gopls'
+    if vim.fn.executable(executable) == 1 then
+      vim.lsp.start({
+        name = 'Go Language Server',
+        cmd = { executable },
+        root_dir = vim.fs.dirname(vim.fs.find({ "go.work", "go.mod", ".git" }, { upward = true })[1]),
+        capabilities = capabilities,
+      })
+    end
+  end
 })
