a5f5d46639578da7ebf6e7ff36313deefcbd102b
CodeVision <codevision@gmail.com>
2023-11-25 09:21:54 +0100

    config(neovim): update neotree settings

--- neovim/.config/nvim/lua/codevision/plugins/neo-tree-nvim.lua
+++ neovim/.config/nvim/lua/codevision/plugins/neo-tree-nvim.lua
@@ -1,5 +1,20 @@
 vim.keymap.set('n', '<leader>n', '<cmd>Neotree toggle<cr>')
 
+require('neo-tree').setup({
+  sources = {
+    "filesystem",
+    "buffers",
+  },
+  event_handlers = {
+    {
+      event = "neo_tree_buffer_enter",
+      handler = function (arg)
+        vim.opt_local.relativenumber = true
+      end
+    }
+  }
+})
+
 -- vim.cmd([[
 -- highlight! link NeoTreeDirectoryIcon NvimTreeFolderIcon
 -- highlight! link NeoTreeDirectoryName NvimTreeFolderName
