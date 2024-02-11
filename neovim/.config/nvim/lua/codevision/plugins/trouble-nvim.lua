b1b886a117d73e530711332aee6c02bd6959c8a2
CodeVision <codevision@gmail.com>
2024-02-11 18:36:44 +0100

    neovim: change diagnostics bindings

--- neovim/.config/nvim/lua/codevision/plugins/trouble-nvim.lua
+++ neovim/.config/nvim/lua/codevision/plugins/trouble-nvim.lua
@@ -1,8 +1,8 @@
 require('trouble').setup({})
 
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true })
-vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { silent = true })
-vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { silent = true })
-vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true })
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true })
+vim.keymap.set("n", "<leader>dx", "<cmd>TroubleToggle<cr>", { silent = true })
+vim.keymap.set("n", "<leader>dw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { silent = true })
+vim.keymap.set("n", "<leader>dd", "<cmd>TroubleToggle document_diagnostics<cr>", { silent = true })
+vim.keymap.set("n", "<leader>dc", "<cmd>TroubleToggle loclist<cr>", { silent = true })
+vim.keymap.set("n", "<leader>dq", "<cmd>TroubleToggle quickfix<cr>", { silent = true })
 vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true })
