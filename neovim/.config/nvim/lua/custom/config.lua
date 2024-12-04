ce52aea0640f92770e7df9e74c098d6d1ba73fd2
CodeVision <codevision@gmail.com>
2024-12-04 14:55:58 +0100

    feat(neovim): add custom typescript lsp config

---
+++ neovim/.config/nvim/lua/custom/config.lua
@@ -0,0 +1,1 @@
+vim.g.node_host_prog = vim.fn.fnamemodify(vim.env.NEOVIM_NODE, ':p:h') .. '/bin/neovim-node-host'
--- neovim/.config/nvim/lua/custom/plugins/lsp/servers/typescript.lua
+++ neovim/.config/nvim/lua/custom/plugins/lsp/servers/typescript.lua
@@ -1,5 +1,22 @@
-local capabilities = require('custom.plugins.lsp.util').capabilities
+local is_vue_project = require('custom.plugins.lsp.util').is_vue_project
 
-require('lspconfig').tsserver.setup({
-  capabilities = capabilities
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'typescript', 'javascript' },
+  callback = function()
+    local ts_executable = vim.fn.fnamemodify(vim.env.NEOVIM_NODE, ':p:h') .. '/bin/typescript-language-server'
+    if vim.fn.executable(ts_executable) == 1 then
+      local root_dir = vim.fs.dirname(vim.fs.find({ 'tsconfig.json', 'package.json', 'jsconfig.json', '.git' }, { upward = true })[1])
+      if is_vue_project(root_dir) then
+        return
+      end
+      vim.lsp.start({
+        name = 'TypeScript Language Server',
+        cmd = { ts_executable, '--stdio' },
+        root_dir = root_dir,
+        init_options = {
+          hostInfo = "neovim"
+        },
+      })
+    end
+  end
 })
