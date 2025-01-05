0698db6bcf4df1d0278219bf0c833e098bfe547a
CodeVision <codevision@gmail.com>
2025-01-05 10:57:11 +0100

    fix(neovim): fix epoch snippet

--- neovim/.config/nvim/lua/custom/snippets/all.lua
+++ neovim/.config/nvim/lua/custom/snippets/all.lua
@@ -10,7 +10,7 @@ ls.add_snippets('all', {
       return os.date('!%Y-%m-%dT%H:%M:%S.000Z', os.time())
     end)
   ),
-  s('curepoch', f(function() return os.time() end)),
+  s('curepoch', f(function() return tostring(os.time()) end)),
   s('uuid', f(function()
     local result = vim.system({ 'uuidgen' }, { text = true }):wait()
     return result.stdout:sub(1, -2)
