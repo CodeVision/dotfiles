a09c1bdf4fa11f96e8727a7626f21ca98aef1adc
CodeVision <git@email.codevision.nl>
2026-09-08 20:51:46 +0200

    config(nvim): update whatodo settings

--- config/neovim/lua/plugins/whatodo.lua
+++ config/neovim/lua/plugins/whatodo.lua
@@ -1,33 +1,74 @@
 require('whatodo').setup({
-  -- Set to true to prompt for filename instead of auto-generating
-  prompt_for_filename = false,
-
   templates = {
     {
       name = "Quick Note",
-      directory = "~/data/notes/Inbox/",
+      directory = "~/data/notes/Inbox/quick",
       filename_pattern = "quick-%Y%m%d-%H%M%S.md",
       content = "# Quick Note\n\nDate: " .. os.date("%Y-%m-%d %H:%M") .. "\n\n",
-      filetype = "markdown",
     },
     {
       name = "Meeting Notes",
-      directory = "~/data/notes/Inbox/meetings/",
+      directory = "~/data/notes/meetings",
       filename_pattern = "meeting-%Y%m%d.md",
       content = "# Meeting Notes\n\nDate: " .. os.date("%Y-%m-%d %H:%M") .. "\nAttendees: \n\n## Agenda\n\n- \n\n## Notes\n\n\n## Action Items\n\n- [ ] \n",
-      filetype = "markdown",
+    }
+  },
+  notes = {
+    refile_directory = '~/data/notes',
+  },
+  todo = {
+    root_dir = '~/data/todo',
+    capture_file = '~/data/todo/refile.wtd',
+    states = {
+      { name = 'TODO',      key = 't' },
+      { name = 'NEXT',      key = 'n' },
+      { name = 'PROGRESS',  key = 'p' },
+      { name = 'WAITING',   key = 'w',},
+      { name = 'DONE',      key = 'd', end_state = true },
+      { name = 'CANCELLED', key = 'c', end_state = true },
+    },
+    end_state_visible_days = 3,
+    deadline_soon_days = 14,
+    keymaps = {
+      schedule = '<leader>ts',
+      deadline = '<leader>td',
+      state = '<leader>tt',
     },
   },
-  planning = {
-    base_dir = '~/data/notes/Planning',
+  prompt_for_filename = false,
+  keymaps = {
+    refile = '<leader>wr',
+  },
+  pomodoro = {
+    work_duration  = 25,
+    break_duration = 5,
     keymaps = {
-      prev    = '<leader>pp',
-      next    = '<leader>pn',
-      current = '<leader>pc',
-      pull    = '<leader>po',
+      start  = '<leader>ps',
+      stop   = '<leader>pq',
+      resume = '<leader>pr',
+    },
+  },
+  journal = {
+    base_dir = '~/data/notes/Personal/Journal/',
+    keymaps = {
+      prev    = '[j',
+      next    = ']j',
+      current = '<leader>wc',
+      pull    = '<leader>wo',
+      refile  = '<leader>wr',
+    },
+    goto = {
+      daily     = '<leader>jd',
+      weekly    = '<leader>jw',
+      monthly   = '<leader>jm',
+      quarterly = '<leader>jq',
+      yearly    = '<leader>jy',
     },
   },
 })
 
 vim.keymap.set('n', '<leader>n', '<cmd>WhatodoNew<cr>', { desc = 'New note from template' })
-vim.keymap.set('n', '<leader>p', '<cmd>WhatodoPlan<cr>', { desc = 'Open planning' })
+vim.keymap.set('n', '<leader>jt', '<cmd>WhatodoJournal<cr>', { desc = 'Open journal' })
+vim.keymap.set('n', '<leader>wa', '<cmd>WhatodoAgenda<cr>', { desc = 'Open WhatodoAgenda'})
+vim.keymap.set('n', '<leader>wt', '<cmd>WhatodoTodos<cr>', { desc = 'Open WhatodoTodos' })
+vim.keymap.set('n', '<leader>wn', '<cmd>WhatodoTodo<cr>', { desc = 'Open Whatodo Capture' })
